3d7d483ba15f7bd70857254b05e83c0b963fc014
cryptoPanda430 <panda.kk1114@gmail.com>
2022-05-17 19:09:59 +0100

    Update

--- migrations/1_deploy_token.js
+++ migrations/1_deploy_token.js
@@ -1,7 +1,7 @@
 // deploy GYSR token
 
-const GeyserToken = artifacts.require('BurnableERC20');
+const GeyserToken = artifacts.require('GeyserToken');
 
 module.exports = function (deployer) {
-    deployer.deploy(GeyserToken, 'name','symbol', "3", "0x30000",  "0x923B2373A81D52F81930aF7dCd784393CD96Ed1B");
+    deployer.deploy(GeyserToken);
 }
